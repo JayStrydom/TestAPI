8c95385bf9ca1ebdccf338de14c961e280dd4b18
Jay Strydom <Jay.strydom@rhipe.com>
2023-08-22 16:28:49 +1200

    Added the port exhaustion and memory leak examples

---
+++ TestAPI/Controllers/MemoryLeakController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Concurrent;
+
+namespace TestAPI.Controllers
+{
+    // Examples: https://github.com/sebastienros/memoryleak
+
+    [Route("[controller]")]
+    [ApiController]
+    public class MemoryLeakController : ControllerBase
+    {
+
+        /// <summary>
+        /// Transient example: memory grows temporary until the GC collection gen 0 instances about every 2 sec
+        /// Note: as long as the CPU is not over-utilized, the garbage collection can deal with a high number of allocations
+        /// </summary>
+        [HttpGet("transient")] // memoryleak/transient
+        public ActionResult<string> GetTransient()
+        {
+            return new String('x', 1000 * 1024);
+        }
+
+        private static ConcurrentBag<string> _staticStrings = new ConcurrentBag<string>();
+
+        /// <summary>
+        /// Managed memory leak example: new objects are held forever so that the GC can't release it
+        /// </summary>
+        [HttpGet("managed")] // memoryleak/managed
+        public ActionResult<string> GetManaged()
+        {
+            var bigString = new String('x', 1000 * 1024);
+            _staticStrings.Add(bigString);
+            return bigString;
+        }
+
+    }
+}
---
+++ TestAPI/Controllers/PortExhaustionController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TestAPI.Controllers
+{
+    // Examples: https://github.com/sebastienros/memoryleak
+
+    [Route("[controller]")]
+    [ApiController]
+    public class PortExhaustionController : ControllerBase
+    {
+
+        /// <summary>
+        /// Create the HTTP object every time instead of reusing it, leading to port exhaustion
+        /// </summary>
+        [HttpGet()] // portexhaustion?url=https://www.google.com
+        public async Task<int> GetTransient([FromQuery] string url)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                var result = await httpClient.GetAsync(url);
+                return (int)result.StatusCode;
+            }
+        }
+    }
+}
