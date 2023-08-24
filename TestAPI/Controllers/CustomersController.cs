b4feb390580ea4999d0bebf627625557277fe9ee
Jay Strydom <Jay.strydom@rhipe.com>
2023-08-25 09:18:36 +1200

    Added the no tracking method

--- TestAPI/Controllers/CustomersController.cs
+++ TestAPI/Controllers/CustomersController.cs
@@ -51,6 +51,44 @@ namespace TestAPI.Controllers
             return model;
         }
 
+        // GET: customers/syncnotracking
+        [HttpGet("syncnotracking")]
+        public CustomerInfoModel GetCustomersNoTracking()
+        {
+            var model = new CustomerInfoModel();
+            var customers = new List<CustomerModel>();
+
+            // dumb queries to excercise the DB
+            model.Total = _context.Customers.AsNoTracking().Count(c => c.CustomerID != 0);
+            var salesOrderTotal = _context.SalesOrders.AsNoTracking().Count(c => c.Customer.CustomerID == 29485);
+
+            var records = _context.SalesOrders
+                            .Include(c => c.Customer)
+                            .Where(so => so.SalesOrderID > 0)
+                            .OrderByDescending(so => so.OrderDate)
+                            .Take(50)
+                            .AsNoTracking();
+
+            foreach (var record in records)
+            {
+                customers.Add(new CustomerModel()
+                {
+                    CustomerID = record.CustomerID,
+                    Title = record.Customer.Title,
+                    FirstName = record.Customer.FirstName,
+                    MiddleName = record.Customer.MiddleName,
+                    LastName = record.Customer.LastName,
+                    CompanyName = record.Customer.CompanyName,
+                    SalesPerson = record.Customer.SalesPerson,
+                    EmailAddress = record.Customer.EmailAddress
+                });
+            }
+
+            model.Customers = customers;
+
+            return model;
+        }
+
         // GET: customers/async
         [HttpGet("async")]
         public async Task<CustomerInfoModel> GetCustomersAsync()
